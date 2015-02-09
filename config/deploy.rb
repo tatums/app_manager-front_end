fe4e1b0897928c404f970562b307f8d6c821f893
tatums <tatum@ashlandstudios.com>
2015-02-08 21:40:34 -0600

    fix linked paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :pty, true
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'public')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
